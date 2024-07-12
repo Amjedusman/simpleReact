af82aca14585c31a99b24e0654918439c43387be
AMJED USMAN <amjadusman98@gmail.com>
2024-07-12 18:20:26 +0530

    some changes are made

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import Header from './components/Header';
+
 
 function App() {
+
+  let count = 0;
+  const data ="njananu map"
+  const pass ="vanno vannu"
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <HELLO/>
+      <p>this is count {count}</p>
+      <h2 className="hello">{data}</h2>
+      <Header pass={pass} />
+
     </div>
   );
 }
 
 export default App;
+
+function HELLO(){
+  return(
+    <h1 style={{color:'red'}}>HELLO WORLD</h1>
+  )
+}
